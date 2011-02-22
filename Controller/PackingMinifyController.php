65ed2d8b68b38c26f318c28408bab98c69cb7f5d
Francis Besset <francis.besset@free.fr>
2011-02-22 13:54:40 +0100

    Updated controller

--- Controller/PackingMinifyController.php
+++ Controller/PackingMinifyController.php
@@ -3,6 +3,7 @@
 namespace BeSimple\PackingMinifyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class PackingMinifyController extends Controller
 {
@@ -12,12 +13,12 @@ class PackingMinifyController extends Controller
 
         if (false !== $content = $this->get('packing_minify')->get($file, $format)) {
             if ($format == 'css') {
-                return $this->createResponse($content, 200, array('Content-Type' => 'text/css'));
+                return new Response($content, 200, array('Content-Type' => 'text/css'));
             } else {
-                return $this->createResponse($content, 200, array('Content-Type' => 'application/x-javascript'));
+                return new Response($content, 200, array('Content-Type' => 'application/x-javascript'));
             }
         }
 
-        return $this->createResponse('', 404);
+        return new Response('', 404);
     }
 }
